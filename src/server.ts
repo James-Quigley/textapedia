63d28df16b6853584f1f3f537eeff96027d162cc
James Quigley <James.A.Quigley@asu.edu>
2017-09-10 15:02:42 -0700

    Functioning large summary

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 /**
  * Module dependencies
  */
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as twilioConfig from "./config/twilio.config";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as twilioConfig from './config/twilio.config';
 
-const twilio = require("twilio");
+const wiki = require('wikijs').default;
+
+const twilio = require('twilio');
 
 /**
  * Controllers (route handlers).
@@ -19,33 +21,37 @@ const client = new twilio(twilioConfig.ACCOUNT_SID, twilioConfig.AUTH_TOKEN);
 
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
  * Start Express server.
  */
-app.listen(app.get("port"), () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    );
-    console.log("  Press CTRL-C to stop\n");
+app.listen(app.get('port'), () => {
+  console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
 });
 
-app.post("/", (req, res) => {
-    console.log(req.body);
-    client.messages
-        .create({
+app.post('/', (req, res) => {
+  const term = req.body.Body;
+
+  wiki()
+    .page(term)
+    .then((page: any) => {
+      //   console.log(page);
+      page.summary().then((summary: any) => {
+        client.messages
+          .create({
             from: twilioConfig.NUMBER,
             to: req.body.From,
-            body: "Received your text!"
-        })
-        .then((message: any) => {
-            console.log(message);
-        });
+            body: page.raw.title + ': ' + summary,
+          })
+          .then((message: any) => {
+            // console.log(message);
+          });
+      });
+    });
 });
 
 module.exports = app;
